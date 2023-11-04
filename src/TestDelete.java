8f38ff17d80f09a829ba7172dae05c5960377042
流浪猫 <116098180+rovermao@users.noreply.github.com>
2023-11-04 16:19:31 +0800

    first commit

---
+++ src/TestDelete.java
@@ -0,0 +1,20 @@
+public class TestDelete {
+    private String filename;
+    private Integer filenum;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Integer getFilenum() {
+        return filenum;
+    }
+
+    public void setFilenum(Integer filenum) {
+        this.filenum = filenum;
+    }
+}
